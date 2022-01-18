4ff246e2ab210a0a57704a0b7adb52ed0bd74732
aleriza <aleriza.sultanova.on@gmail.com>
2022-01-19 00:57:10 +0600

    add task4

---
+++ js/task4.js
@@ -0,0 +1,17 @@
+//let a = 4;
+//let b = 5;
+//console.log(a + b);
+
+/*let a = 5;
+let b = 5;
+console.log(a * b);
+*/
+
+/*let a = 9;
+let b = 4;
+console.log(a % b);
+*/
+
+let a = 6;
+let b = 2;
+console.log(a ** b);
